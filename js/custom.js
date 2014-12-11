1e55ae35df503a08ad6d7ca07c4b4a03da61ae64
Ryan Ernst <rernst76@gmail.com>
2014-12-11 22:51:57 +0000

    Implemented movement filtering

--- js/custom.js
+++ js/custom.js
@@ -1,5 +1,8 @@
-// Modify dropdown to change value on click
-$(".dropdown-menu li a").click(function(){
-  $(this).parents(".dropdown").find('.btn').text($(this).text());
-  $(this).parents(".dropdown").find('.btn').val($(this).text());
+// Code to filter movement list
+var $products = $('#movementList li');
+$('#movementFilter').keyup(function() {
+    var re = new RegExp($(this).val(), "i"); // "i" means it's case-insensitive
+    $products.show().filter(function() {
+        return !re.test($(this).text());
+    }).hide();
 });
