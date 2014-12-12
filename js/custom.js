332eac62ce75890c4263fa30467b3f439d7e9ef3
Ryan Ernst <rernst76@gmail.com>
2014-12-12 19:54:33 +0000

    Added border effect when item is dragged over

--- js/custom.js
+++ js/custom.js
@@ -7,17 +7,43 @@ $('#movementFilter').keyup(function() {
     }).hide();
 });
 
-
+// Function to handle dragstart event
 function handleDragStart(e) {
-    e.dataTransfer.effectAllowed='copy';
+    e.dataTransfer.effectAllowed = 'copy';
     this.style.opacity = '0.4';
     e.dataTransfer.setData("text/html", e.target);
 }
 
-// Add listeners for dragstart
+// Function to handle dragenter
+function handleDragEnter(e) {
+    e.target.querySelector('.itemDragAreaText')
+      .style.visibility = "hidden";
+    this.classList.add("dragover");
+}
+
+// Function ot handle dragover
+function handleDragOver(e) {
+    e.preventDefault(); // Necessary to allow drop
+    e.dataTransfer.dropEffect = 'copy';
+}
+
+// Function to handle dragleave
+function handleDragLeave(e) {
+    e.target.querySelector('.itemDragAreaText')
+      .style.visibility = "visible";
+    this.classList.remove("dragover");
+}
+
+// Add listeners to draggable items
 var items = document.querySelectorAll(".xitem, .mitem");
 [].forEach.call(items, function(item) {
-   item.addEventListener('dragstart', handleDragStart, false); 
+    item.addEventListener('dragstart', handleDragStart, false);
 });
 
-// 
+// Add listeners to drop areas
+items = document.querySelectorAll(".itemDragArea");
+[].forEach.call(items, function(item) {
+    item.addEventListener('dragenter', handleDragEnter, false);
+    item.addEventListener('dragover', handleDragOver, false);
+    item.addEventListener('dragleave', handleDragLeave, false);
+});
