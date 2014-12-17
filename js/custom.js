0e8c36c19214b5fcc9f880cbdd2d98efb7177403
Ryan Ernst <rernst76@gmail.com>
2014-12-17 22:42:35 +0000

    Added check in dragenter and dragover to make sure we are in itemDragArea

--- js/custom.js
+++ js/custom.js
@@ -17,6 +17,7 @@ var dnd = {
 
 // Function to handle dragstart event
 function handleDragStart(e) {
+    logEvent(e);
     
     //Set action based on stockitem class
     dnd.action  = (e.target.classList.contains("stockitem")) ? "copy" : "move";
@@ -31,40 +32,48 @@ function handleDragStart(e) {
 
 // Function to handle dragenter
 function handleDragEnter(e) {
-    e.preventDefault();
-    e.stopPropagation();
-    
-    // Add dragover class to e.target
-    $(this).addClass("dragover");
-    
-    // Check if entered element contains drag area text, hide it
-    var dragAreaText = $(this).children(".itemDragAreaText");
-    if (dragAreaText) {
-        dragAreaText.css("visibility", "hidden");
+    if($(this).hasClass("itemDragArea")) {
+        e.preventDefault();
+        e.stopPropagation();
+        logEvent(e);
+        
+        // Add dragover class to e.target
+        $(this).addClass("dragover");
+        
+        // Check if entered element contains drag area text, hide it
+        var dragAreaText = $(this).children(".itemDragAreaText");
+        if (dragAreaText) {
+            dragAreaText.css("visibility", "hidden");
+        }
     }
 }
 
 // Function to handle dragover
 function handleDragOver(e) {
-    e.preventDefault(); // Necessary to allow drop
+    if($(e.target).hasClass("itemDragArea"))
+        e.preventDefault();
     e.stopPropagation();
 }
 
 // Function to handle dragleave
 function handleDragLeave(e) {
-    e.stopPropagation();
-    // Remove dragover class from e.target
-    $(this).removeClass("dragover");
-    
-    // Check if left element contains drag area text, show it
-    var dragAreaText = $(this).children(".itemDragAreaText");
-    if (dragAreaText) {
-        dragAreaText.css("visibility", "visible");
+    if($(this).hasClass("itemDragArea")) {
+        logEvent(e);
+        
+        // Remove dragover class
+        $(this).removeClass("dragover");
+        
+        // Check if element contains drag area text, show it
+        var dragAreaText = $(this).children(".itemDragAreaText");
+        if (dragAreaText) {
+            dragAreaText.css("visibility", "visible");
+        }
     }
 }
 
 // Function to handle drop event
 function handleDrop(e) {
+    logEvent(e);
     e.preventDefault();
     
     // Don't let this bubble, or we will have problems with nesting
@@ -96,8 +105,7 @@ function handleDrop(e) {
     });
     
     if (dnd.action === "move") {
-        // Pre-emptively replace drag-text if needed
-        console.log($(dnd.dragElement).parent().parent()[0]);
+        // Pre-emptively replace drag-text if needed, make sure it needs it
         var isPermArea = $(dnd.dragElement).parent().parent().hasClass('permArea');
         if($(dnd.dragElement).parent().children().length <= 1 && !isPermArea) {
             $(dnd.dragElement).parent().parent().append('<p class="itemDragAreaText">Drop items here</p>');
@@ -123,6 +131,7 @@ function handleDrop(e) {
 
 // Function to handle drag end
 function handleDragEnd(e) {
+    logEvent(e);
     dnd.dragElement.style.opacity = '1';
 }
 
@@ -148,4 +157,9 @@ function insertDropZones(node) {
 // Removes drop zone at first and last element position inside of node
 function removeDropZones(node) {
     
+}
+
+function logEvent(e) {
+    console.log(e.type);
+    console.log(e.originalEvent.target);
 }
